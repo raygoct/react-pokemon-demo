3de497ea635692e9470317ea13edc660bcf275e4
Raymond Gonzalez <rgonzalez@crunchtime.com>
2024-08-07 09:44:05 -0400

    Remove browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import {BrowserRouter} from 'react-router-dom'
 import {Container} from '@mui/material'
 
 const App: React.FC = () => {
     return (
-        <BrowserRouter>
-            <Container component="main" sx={{mt: 4}}>
-            </Container>
-        </BrowserRouter>
+        <Container>
+        </Container>
     )
 }
 
